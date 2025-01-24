02447a9860ab07b596fcea545d3b98dd602513f3
Godly-Biju <godlybiju0.com>
2025-01-24 11:50:03 +0530

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -151,7 +151,31 @@ function App() {
           </tr>
           ))
         }
-          
+        <tr>
+          <td>1</td>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+            
+          ))}
+          <td><i class="fa-solid fa-pen-to-square"></i></td>
+          <td><i class="fa-solid fa-delete-left"></i></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+          <td><i class="fa-solid fa-pen-to-square"></i></td>
+          <td><i class="fa-solid fa-delete-left"></i></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+          <td><i class="fa-solid fa-pen-to-square"></i></td>
+          <td><i class="fa-solid fa-delete-left"></i></td>
+        </tr>
         </tbody>
       </Table>
     </>
